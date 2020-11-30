1ac6aacc0f5d513c32b55df881e958635f861f77
CocaCola433 <57605281+CocaCola433@users.noreply.github.com>
2020-11-30 14:57:16 +0800

    Create 113001.继承父类.R.java
    ...

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,9 @@
+
+public class App extends R{
+
+	public static void main(String[] args) {
+		System.out.println(R.boy_filePath);
+		System.out.println(R.girl_filePath);
+		System.out.println(R.array2d);
+	}
+}
