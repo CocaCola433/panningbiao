233f6a18d9c63a101eb70950a3a3a12b05bb3079
CocaCola433 <57605281+CocaCola433@users.noreply.github.com>
2020-11-30 15:31:07 +0800

    Update 113002.实现接口.App.java
    ...

--- 113002.实现接口.App.java
+++ 113002.实现接口.App.java
@@ -3,6 +3,12 @@ public class App implements R{
 	public static void main(String[] args) {
 		System.out.println(R.boy_filePath);
 		System.out.println(R.girl_filePath);
-		System.out.println(R.array2d);
+		
+		for (int row = 0; row < R.array2d.length; row++) {
+			for (int column = 0; column < R.array2d[0].length; column++) {
+				System.out.print(R.array2d[row][column]+" ");
+			}
+		 System.out.println();
+		}
 	}
 }
