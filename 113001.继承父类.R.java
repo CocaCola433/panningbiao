71ac23629eeb78c63e7f92fca8e85924aa500bec
CocaCola433 <57605281+CocaCola433@users.noreply.github.com>
2020-11-30 15:08:03 +0800

    Update 113001.继承父类.R.java
    ...

--- 113001.继承父类.R.java
+++ 113001.继承父类.R.java
@@ -1,9 +1,9 @@
 
 public class R {
 	
-	String boy_filePath="img/boy.gif";
-	String girl_filePath="img/girl.gif";
-	int [ ][ ]array2d={{1,2,3},{4,5,6}};
+	static String boy_filePath="img/boy.gif";
+	static String girl_filePath="img/girl.gif";
+	static int [ ][ ]array2d={{1,2,3},{4,5,6}};
 
 
 }
