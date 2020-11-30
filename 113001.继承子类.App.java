1c3dba2651cd7f7d97649945bb0143d02266d73f
CocaCola433 <57605281+CocaCola433@users.noreply.github.com>
2020-11-30 15:28:08 +0800

    Update 113001.继承子类.App.java
    ...

--- 113001.继承子类.App.java
+++ 113001.继承子类.App.java
@@ -1,8 +1,14 @@
 public class App extends R{
 
 	public static void main(String[] args) {
+		
 		System.out.println(boy_filePath);
 		System.out.println(girl_filePath);
-		System.out.println(array2d[0][0]);
+		for (int row = 0; row < array2d.length; row++) {
+			for (int column = 0; column < array2d[0].length; column++) {
+				System.out.print(array2d[row][column]+" ");
+			}
+		 System.out.println();
+		}
 	}
 }
